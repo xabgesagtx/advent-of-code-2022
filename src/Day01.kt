fda1154f6c882aa4988a0cb3f707d0cfe3d325d3
xabgesagtx <xabgesagtx@riseup.net>
2022-12-01 22:29:45 +0100

    Day 1: simple attempt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
+
+    fun readElves(input: List<String>): MutableMap<Int, Int> {
+        var currentElf = 1
+        val elves = mutableMapOf<Int, Int>()
+        input.forEach {
+            if (it.isBlank()) {
+                currentElf++
+            } else {
+                elves.merge(currentElf, it.toInt()) { first, second -> first + second }
+            }
+        }
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = readElves(input)
+        return elves.map { it.value }.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return readElves(input)
+            .map { it.value }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
