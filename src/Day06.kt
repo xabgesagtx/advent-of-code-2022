65040203c0c7bf395edcf4ea28100f84f27db175
xabgesagtx <xabgesagtx@riseup.net>
2022-12-06 12:55:27 +0100

    Day 6: Fist attempt

---
+++ src/Day06.kt
@@ -0,0 +1,28 @@
+fun main() {
+
+    fun checkForMarker(input: String, distinctCharacters: Int): Int {
+        return input.toList()
+            .withIndex()
+            .windowed(distinctCharacters)
+            .first { window -> window.map { it.value }.toSet().size == distinctCharacters }
+            .last()
+            .index + 1
+    }
+
+    fun part1(input: String): Int {
+        return checkForMarker(input, 4)
+    }
+
+    fun part2(input: String): Int {
+        return checkForMarker(input, 14)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+
+    val input = readInput("Day06").first()
+    println(part1(input))
+    println(part2(input))
+}
