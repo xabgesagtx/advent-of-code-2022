6d199beb6c52853e2e58b0049231291af57d3000
xabgesagtx <xabgesagtx@riseup.net>
2022-12-07 20:58:41 +0100

    Day 7: Small fix and formatting

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,43 +1,42 @@
 fun main() {
 
     fun readDirectoryContent(input: List<String>): FSDir {
-        return input.fold(listOf<FSDir>()) {
-            stack, line ->
-                when {
-                    line.isLeaveCommand -> stack.dropLast(1)
-                    line.isChangeDir ->  {
-                        val newDir = line.toDir()
-                        stack.lastOrNull()?.addItem(newDir)
-                        stack + newDir
-                    }
-                    line.isFile -> {
-                        stack.last().addItem(line.toFile())
-                        stack
-                    }
-                    else -> stack
+        return input.fold(listOf<FSDir>()) { stack, line ->
+            when {
+                line.isLeaveCommand -> stack.dropLast(1)
+                line.isChangeDir -> {
+                    val newDir = line.toDir()
+                    stack.lastOrNull()?.addItem(newDir)
+                    stack + newDir
                 }
+                line.isFile -> {
+                    stack.last().addItem(line.toFile())
+                    stack
+                }
+                else -> stack
+            }
         }.first()
     }
 
     fun part1(input: List<String>): Int {
         val mainDir = readDirectoryContent(input)
         mainDir.print()
-        return mainDir.allDirs.map { it.calculatedSize() }.filter { it < 100000 }.sum()
+        return mainDir.allDirs.map { it.calculatedSize() }.filter { it <= 100_000 }.sum()
     }
 
     fun part2(input: List<String>): Int {
         val mainDir = readDirectoryContent(input)
         val spaceUsed = mainDir.calculatedSize()
-        val spaceFree = 70000000 - spaceUsed
-        val spaceToBeFreed = 30000000 - spaceFree
-        return mainDir.allDirs.map { it.calculatedSize() }.filter { it > spaceToBeFreed }.min()
+        val spaceFree = 70_000_000 - spaceUsed
+        val spaceToBeFreed = 30_000_000 - spaceFree
+        return mainDir.allDirs.map { it.calculatedSize() }.filter { it >= spaceToBeFreed }.min()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     println(part1(testInput))
-    check(part1(testInput) == 95437)
-    check(part2(testInput) == 24933642)
+    check(part1(testInput) == 95_437)
+    check(part2(testInput) == 24_933_642)
 
     val input = readInput("Day07")
     println(part1(input))
@@ -76,14 +75,14 @@ private data class FSFile(val size: Int, val name: String) : FSItem {
     }
 }
 
-private data class FSDir(val name: String): FSItem {
+private data class FSDir(val name: String) : FSItem {
     private val items: MutableList<FSItem> = mutableListOf()
 
     override fun calculatedSize(): Int = items.sumOf { it.calculatedSize() }
 
     override fun print(indent: Int) {
         println("${" ".repeat(indent)} dir $name")
-        items.forEach{
+        items.forEach {
             it.print(indent + 2)
         }
     }
